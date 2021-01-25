0a56c9a86680c04b4a9bb30d0394becd11291e1e
Victor Uwaje <40861452+Victoloporsche@users.noreply.github.com>
2021-01-25 16:54:43 +0100

    add pytorch model to main file

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from data_exploratory import DataExploratory
 from model_optimizer import ModelOptimizer
+from pytorch_regression_model import PytorchRegression
 
 
 class Main:
     def __init__(self, train, test, id_column, y_column_name, num_of_features_to_select):
         self.exploratory = DataExploratory(train, test)
         self.model = ModelOptimizer(train, test, id_column, y_column_name, num_of_features_to_select)
+        self.pytorch = PytorchRegression(train, test, id_column, y_column_name, num_of_features_to_select)
 
     def check_missing_values(self):
         return self.exploratory.get_missing_values()
@@ -27,6 +29,12 @@ class Main:
 
     def optimize_and_train_model(self, predicted_column_name):
         return self.model.optimize_train_model(predicted_column_name)
+
+    def train_with_pytorch(self):
+        return self.pytorch.train_model_with_torch()
+
+    def prediction_pytorch(self):
+        return self.pytorch.model_prediction_pytorch()
 
 
 
